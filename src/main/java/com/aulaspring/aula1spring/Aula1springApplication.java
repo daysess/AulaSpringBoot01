789cb203aba6ace9112f81311d96a7ed6c45de32
Dayse Soares <daysedesign@gmail.com>
2019-11-14 13:56:50 -0300

    Criar projeto Spring Boot Java

---
+++ src/main/java/com/aulaspring/aula1spring/Aula1springApplication.java
@@ -0,0 +1,13 @@
+package com.aulaspring.aula1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula1springApplication.class, args);
+	}
+
+}
